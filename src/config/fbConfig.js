806a683f0bff67a362dd2d45b6f9e4c22090ebaf
unknown <hmitpg94@gmail.com>
2021-04-28 10:13:01 +0530

    Adding firebase config file

---
+++ src/config/fbConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+var config = {
+    apiKey: "AIzaSyA5xQs4wwY11h32GNmsRL5NglWIjMe7r0A",
+    authDomain: "onl-service-desk.firebaseapp.com",
+    projectId: "onl-service-desk",
+    storageBucket: "onl-service-desk.appspot.com",
+    messagingSenderId: "649016646411",
+    appId: "1:649016646411:web:91520db34a31d84036600e",
+    measurementId: "G-29DB0BYMTX"
+};
+// Initialize Firebase
+firebase.initializeApp(config);
+firebase.firestore().settings({ timeStampInSnapshot: true });
+
+export default firebase;
